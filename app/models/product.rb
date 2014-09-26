46f1d1b903246d3f27dc0573fde03a4fcde9c5d5
Alexander Kehaya <afkehaya@gmail.com>
2014-09-25 20:45:15 -0700

    Add collums to seed file but erb is not showing up

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  subtitle    :string(255)
+#  author      :string(255)
+#  description :text
+#  sku         :string(255)
+#  price       :decimal(, )
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Product < ActiveRecord::Base
+	
 end
